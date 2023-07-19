a05e66cd7d4debc7f05cd929e02d6def2c638b40
Shaik Nayab Rasool <shaiknayabrasool.s@prod.hclpnp.com>
2023-07-19 12:55:48 +0530

    DNEXT-14684 added new samples SECKFMGetIDFlags &SECKFMGetUserInfo

--- samples/misc/idvault/idvault.c
+++ samples/misc/idvault/idvault.c
@@ -53,332 +53,332 @@ int main(int argc, char *argv[])
     /* Local data declarations */
          
     STATUS     error = NOERROR;             /* error code from API calls */
-	KFHANDLE	hKFC = NULLKFHANDLE;
-	KFHANDLE	hKFC2 = NULLKFHANDLE;
+    KFHANDLE	hKFC = NULLKFHANDLE;
+    KFHANDLE	hKFC2 = NULLKFHANDLE;
     DBHANDLE  hDB;
 
-	char    *ServerName;
-	char    *DomainName;
-	char	DBName[]="names.nsf";
-	char    DBPath[MAXPATH+1];
+    char	*ServerName;
+    char	*DomainName;
+    char	DBName[]="names.nsf";
+    char	DBPath[MAXPATH+1];
 #ifdef OS400
-	char	UserS1ID[] = "/DominoServer/LGCToolkit/people/s1.id";
-	char	UserS2ID[] = "/DominoServer/LGCToolkit/people/s2.id";
-	char	UserS3ID[] = "/DominoServer/LGCToolkit/people/s3.id";
-	char	UserSSID[] = "/DominoServer/LGCToolkit/people/ss.id";
+    char	UserS1ID[] = "/DominoServer/LGCToolkit/people/s1.id";
+    char	UserS2ID[] = "/DominoServer/LGCToolkit/people/s2.id";
+    char	UserS3ID[] = "/DominoServer/LGCToolkit/people/s3.id";
+    char	UserSSID[] = "/DominoServer/LGCToolkit/people/ss.id";
 #else
-	char	UserS1ID[] = "s1.id";
-	char	UserS2ID[] = "s2.id";
-	char	UserS3ID[] = "s3.id";
-	char	UserSSID[] = "ss.id";
+    char	UserS1ID[] = "s1.id";
+    char	UserS2ID[] = "s2.id";
+    char	UserS3ID[] = "s3.id";
+    char	UserSSID[] = "ss.id";
 #endif
-	char	UserNameS1[MAXUSERNAME] = "CN=s1/O=";
-	char	UserNameS2[MAXUSERNAME] = "CN=s2/O=";
-	char	UserNameS3[MAXUSERNAME] = "CN=s3/O=";
-	char	UserNameSS[MAXUSERNAME] = "CN=ss/O=";
-	char	PASSWORD[]="123456";
-	DWORD	VaultFlags = 0;  
-	WORD    wFlags = 0;
-  char    UserName[MAXUSERNAME] = { 0 };
-  LICENSEID licenseID;
+    char	UserNameS1[MAXUSERNAME] = "CN=s1/O=";
+    char	UserNameS2[MAXUSERNAME] = "CN=s2/O=";
+    char	UserNameS3[MAXUSERNAME] = "CN=s3/O=";
+    char	UserNameSS[MAXUSERNAME] = "CN=ss/O=";
+    char	PASSWORD[]="123456";
+    DWORD	VaultFlags = 0;  
+    WORD    wFlags = 0;
+    char    UserName[MAXUSERNAME] = { 0 };
+    LICENSEID licenseID;
     
-	if(argc != 3)
-	{
-		PRINTLOG( "\nUsage:  %s  <server name> <domain>\n", argv[0] );
-		return (0);
-	}
+    if(argc != 3)
+    {
+        PRINTLOG( "\nUsage:  %s  <server name> <domain>\n", argv[0] );
+        return (0);
+    }
 
-	/* init ... */
-	ServerName = argv[1];
-	DomainName = argv[2];	
-	strcat(UserNameS1,DomainName);
-	strcat(UserNameS2,DomainName);
-	strcat(UserNameS3,DomainName);
-	strcat(UserNameSS,DomainName);
+    /* init ... */
+    ServerName = argv[1];
+    DomainName = argv[2];	
+    strcat(UserNameS1,DomainName);
+    strcat(UserNameS2,DomainName);
+    strcat(UserNameS3,DomainName);
+    strcat(UserNameSS,DomainName);
 
 
-        if (error = NotesInitExtended (argc, argv))
-        {
-           fprintf (stderr, "\nError initializing Notes.\n");
-           return (1);
-        }
+    if (error = NotesInitExtended (argc, argv))
+    {
+        fprintf (stderr, "\nError initializing Notes.\n");
+        return (1);
+    }
 
-	if ( error = OSPathNetConstruct( NULL, ServerName,DBName,DBPath) ){
-	    PRINTERROR (error,"OSPathNetConstruct");
-	    NotesTerm();
-	    return (1);
-	}
+    if ( error = OSPathNetConstruct( NULL, ServerName,DBName,DBPath) ){
+        PRINTERROR (error,"OSPathNetConstruct");
+        NotesTerm();
+        return (1);
+    }
 
-	/*Must be call NSFDbOpen() for init before execute other id vault functions.*/	
-	if ( error = NSFDbOpen (DBPath, &hDB) ){
-	    PRINTERROR (error,"NSFDbOpen");
-	    NotesTerm();
-	    return (1);
-	}
+    /*Must be call NSFDbOpen() for init before execute other id vault functions.*/	
+    if ( error = NSFDbOpen (DBPath, &hDB) ){
+        PRINTERROR (error,"NSFDbOpen");
+        NotesTerm();
+        return (1);
+    }
 
-	error = SECKFMOpen (&hKFC, UserS1ID, PASSWORD, SECKFM_open_All, 0, NULL);
-	if ( error ){
-	     PRINTERROR (error,"SECKFMOpen");
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-	}
+    error = SECKFMOpen (&hKFC, UserS1ID, PASSWORD, SECKFM_open_All, 0, NULL);
+    if ( error ){
+        PRINTERROR (error,"SECKFMOpen");
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+    }
 
         /*
-	 * Get ID flags information associated to keyfile context.
-	 * SECKFMGetIDFlags - This routine provides access to ID flags
-	 * associated with the keyfile
-	 * context that is passed in. This API can return below flags,
-	 * fIDFH_Password    0x0001 which indicates that file is password protected.
-	 * fIDFH_PWShareable 0x0008 which indicates that password may be shared by all process.
-	 * Information on these flags can be seen in kfm.h file.
-	 *
-	 */
-         error = SECKFMGetIDFlags(hKFC, &wFlags);
-         if ( error ){
-              PRINTERROR (error,"SECKFMGetIDFlags");
-         }
-         else {
-             PRINTLOG("SECKFMGetIDFlags returned ID flags %d.\n", wFlags);
-         }
+    * Get ID flags information associated to keyfile context.
+    * SECKFMGetIDFlags - This routine provides access to ID flags
+    * associated with the keyfile
+    * context that is passed in. This API can return below flags,
+    * fIDFH_Password    0x0001 which indicates that file is password protected.
+    * fIDFH_PWShareable 0x0008 which indicates that password may be shared by all process.
+    * Information on these flags can be seen in kfm.h file.
+    *
+    */
+    error = SECKFMGetIDFlags(hKFC, &wFlags);
+    if ( error ){
+        PRINTERROR (error,"SECKFMGetIDFlags");
+    }
+    else {
+        PRINTLOG("SECKFMGetIDFlags returned ID flags %d.\n", wFlags);
+    }
 
-        if ( fIDFH_Password & wFlags ){
-             PRINTLOG("SECKFMGetIDFlags returned ID as password protected.\n");
-        }
+    if ( fIDFH_Password & wFlags ){
+        PRINTLOG("SECKFMGetIDFlags returned ID as password protected.\n");
+    }
   
-       /* Get user info using SECKFMGetUserInfo API */
-       error = SECKFMGetUserInfo(hKFC, UserName, &licenseID);
-       if ( NOERROR != error ){
-            PRINTERROR (error, "SECKFMGetUserInfo");
-       }
-       else {
-            PRINTLOG("SECKFMGetUserInfo returned username %s.\n", UserName);
-       }
+    /* Get user info using SECKFMGetUserInfo API */
+    error = SECKFMGetUserInfo(hKFC, UserName, &licenseID);
+    if ( NOERROR != error ){
+        PRINTERROR (error, "SECKFMGetUserInfo");
+    }
+    else {
+        PRINTLOG("SECKFMGetUserInfo returned username %s.\n", UserName);
+    }
 
-	/* Put ID file using the hKFC */
-	error = SECidfPut (UserNameS1, 
-			   PASSWORD,
-			   UserS1ID,
-			   &hKFC, 
-			   ServerName,
-			   0,
-			   0,
-			   NULL);
-	
-	if ( error ){
-	     PRINTERROR (error,"SECidfPut");
-	     SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-	}
-	PRINTLOG("Put ID file using the hKFC...OK\n");
-
-	SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
-
-        error = SECKFMOpen (&hKFC, UserS2ID, PASSWORD, SECKFM_open_All, 0, NULL);
-	if ( error ){
-             PRINTERROR (error,"SECKFMOpen");
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-	}
-	/* Put ID file using local file name and hKFC - only hKFC should be used */
- 	error = SECidfPut (UserNameS2, 
-			   PASSWORD,
-			   NULL, 
-			   &hKFC, 
-			   ServerName, 
-			   0, 
-			   0,
-			   NULL);
-	if ( error ){
-             PRINTERROR (error,"SECidfPut");
-	     SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-	}
-
-	PRINTLOG("Put ID file using local file name and hKFC - only hKFC should be used...OK\n");
-	SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
-
-        error = SECKFMOpen (&hKFC, UserS3ID, PASSWORD, SECKFM_open_All, 0, NULL);
-	if ( error ){
-             PRINTERROR (error,"SECKFMOpen");
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-	}
-	
-	/* Put ID file using local file name */
-	error = SECidfPut (UserNameS3, 
-			   PASSWORD,
-			   UserS3ID, 
-			   NULL, 
-			   ServerName, 
-			   0, 
-			   0,
-			   NULL);
-
-	if ( error ){
-             PRINTERROR (error,"SECidfPut");
-             SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-	}
-
-	PRINTLOG("Put ID file using local file name...OK\n");
-	SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
-
-	/* Get id file to local file name and hKFC */
-	error = SECidfGet (UserNameS1, 
-			PASSWORD,
-			UserS1ID, 
-			&hKFC2, 
-			ServerName, 
-			0,
-			0,
-			NULL);
-	if ( error ){
-             PRINTERROR (error,"SECidfGet");
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-	}
-	PRINTLOG("Get id file to local file name and hKFC...OK\n");
-
-
-	SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
-
-
-	/* Get id file to just hKFC*/
-	error = SECidfGet (UserNameS2, 
-			PASSWORD,
-			NULL, 
-			&hKFC2, 
-			ServerName, 
-			0,
-			0,
-			NULL);
-	if ( error ){
-             PRINTERROR (error,"SECidfGet");
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-	}
-
-	PRINTLOG("Get id file to just hKFC...OK\n");
-	SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
-
-
-
-	/* Get id file to just local file name */
-	error = SECidfGet (UserNameS3, 
-			PASSWORD,
-			UserS3ID,  
-			NULL, 
-			ServerName, 
-			0,
-			0,
-			NULL);
-	if ( error ){
-             PRINTERROR (error,"SECidfGet");
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-	}
-
-	PRINTLOG("Get id file to just local file name...OK\n");
-
-	error = SECKFMOpen (&hKFC2, UserS1ID, PASSWORD, SECKFM_open_All, 0, NULL);
-	if ( error ){
-             PRINTERROR (error,"SECKFMOpen");
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-	}
-
-	/* Sync the hKFC with the ID file in the Vault and write new hKFC */
-	error = SECidfSync (UserNameS1, PASSWORD,
-			NULL,
-			&hKFC2, 
-			ServerName, 
-			0, 
-			0,
-			NULL, 
-			&VaultFlags);
-	if ( error ){
-             PRINTERROR (error,"SECidfSync");
-             SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-	}
-	PRINTLOG("Sync the hKFC with the ID file in the Vault and write new hKFC...OK\n");
-
-	/* Sync the ID file with the ID file in the Vault */
-	error = SECidfSync (UserNameS1, PASSWORD,
-			UserS1ID, 
-			NULL,
-			ServerName, 
-			0,
-			0, 
-			NULL, 
-			&VaultFlags);
-        if ( error ){
-             PRINTERROR (error,"SECidfSync");
-             SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-        }
-	PRINTLOG("Sync the ID file with the ID file in the Vault ...OK\n");
-
-	/* Sync the hKFC with the ID file in the Vault and hKFC */
-        error = SECidfSync (UserNameS1, PASSWORD,
-			UserS1ID, 
-			&hKFC2, 
-			ServerName, 
-			0,
-			0,
-			NULL, 
-			&VaultFlags);
-        if ( error ){
-             PRINTERROR (error,"SECidfSync");
-             SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
-             NSFDbClose(hDB);
-             NotesTerm();
-             return (1);
-
-        }
-        PRINTLOG("Sync the hKFC with the ID file in the Vault and hKFC...OK\n");
-
-        SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
-
-
-        /* Terminate Domino and Notes. */
-
-
-
+    /* Put ID file using the hKFC */
+    error = SECidfPut (UserNameS1, 
+                       PASSWORD,
+                       UserS1ID,
+                       &hKFC, 
+                       ServerName,
+                       0,
+                       0,
+                       NULL);
+    
+    if ( error ){
+        PRINTERROR (error,"SECidfPut");
+        SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
         NSFDbClose(hDB);
-
         NotesTerm();
+        return (1);
 
-        /* End of intro program. */
-        PRINTLOG("\nProgram completed successfully.\n");
-        return (0);
+    }
+    PRINTLOG("Put ID file using the hKFC...OK\n");
+
+    SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
+
+    error = SECKFMOpen (&hKFC, UserS2ID, PASSWORD, SECKFM_open_All, 0, NULL);
+    if ( error ){
+        PRINTERROR (error,"SECKFMOpen");
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+    /* Put ID file using local file name and hKFC - only hKFC should be used */
+    error = SECidfPut (UserNameS2, 
+                       PASSWORD,
+                       NULL, 
+                       &hKFC, 
+                       ServerName, 
+                       0, 
+                       0,
+                       NULL);
+    if ( error ){
+        PRINTERROR (error,"SECidfPut");
+        SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+
+    PRINTLOG("Put ID file using local file name and hKFC - only hKFC should be used...OK\n");
+    SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
+
+    error = SECKFMOpen (&hKFC, UserS3ID, PASSWORD, SECKFM_open_All, 0, NULL);
+    if ( error ){
+        PRINTERROR (error,"SECKFMOpen");
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+    
+    /* Put ID file using local file name */
+    error = SECidfPut (UserNameS3, 
+                       PASSWORD,
+                       UserS3ID, 
+                       NULL, 
+                       ServerName, 
+                       0, 
+                       0,
+                       NULL);
+
+    if ( error ){
+        PRINTERROR (error,"SECidfPut");
+        SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+
+    PRINTLOG("Put ID file using local file name...OK\n");
+    SECKFMClose (&hKFC, SECKFM_close_WriteIdFile, 0, NULL);
+
+    /* Get id file to local file name and hKFC */
+    error = SECidfGet (UserNameS1, 
+                       PASSWORD,
+                       UserS1ID, 
+                       &hKFC2, 
+                       ServerName, 
+                       0,
+                       0,
+                       NULL);
+    if ( error ){
+        PRINTERROR (error,"SECidfGet");
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+    PRINTLOG("Get id file to local file name and hKFC...OK\n");
+
+
+    SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
+
+
+    /* Get id file to just hKFC*/
+    error = SECidfGet (UserNameS2, 
+                       PASSWORD,
+                       NULL, 
+                       &hKFC2, 
+                       ServerName, 
+                       0,
+                       0,
+                       NULL);
+    if ( error ){
+        PRINTERROR (error,"SECidfGet");
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+
+    PRINTLOG("Get id file to just hKFC...OK\n");
+    SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
+
+
+
+    /* Get id file to just local file name */
+    error = SECidfGet (UserNameS3, 
+                       PASSWORD,
+                       UserS3ID,  
+                       NULL, 
+                       ServerName, 
+                       0,
+                       0,
+                       NULL);
+    if ( error ){
+        PRINTERROR (error,"SECidfGet");
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+
+    PRINTLOG("Get id file to just local file name...OK\n");
+
+    error = SECKFMOpen (&hKFC2, UserS1ID, PASSWORD, SECKFM_open_All, 0, NULL);
+    if ( error ){
+        PRINTERROR (error,"SECKFMOpen");
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+
+    /* Sync the hKFC with the ID file in the Vault and write new hKFC */
+    error = SECidfSync (UserNameS1, PASSWORD,
+                        NULL,
+                        &hKFC2, 
+                        ServerName, 
+                        0, 
+                        0,
+                        NULL, 
+                        &VaultFlags);
+    if ( error ){
+        PRINTERROR (error,"SECidfSync");
+        SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+    PRINTLOG("Sync the hKFC with the ID file in the Vault and write new hKFC...OK\n");
+
+    /* Sync the ID file with the ID file in the Vault */
+    error = SECidfSync (UserNameS1, PASSWORD,
+                        UserS1ID, 
+                        NULL,
+                        ServerName, 
+                        0,
+                        0, 
+                        NULL, 
+                        &VaultFlags);
+    if ( error ){
+        PRINTERROR (error,"SECidfSync");
+        SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+    PRINTLOG("Sync the ID file with the ID file in the Vault ...OK\n");
+
+    /* Sync the hKFC with the ID file in the Vault and hKFC */
+    error = SECidfSync (UserNameS1, PASSWORD,
+                        UserS1ID, 
+                        &hKFC2, 
+                        ServerName, 
+                        0,
+                        0,
+                        NULL, 
+                        &VaultFlags);
+    if ( error ){
+        PRINTERROR (error,"SECidfSync");
+        SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
+        NSFDbClose(hDB);
+        NotesTerm();
+        return (1);
+
+    }
+    PRINTLOG("Sync the hKFC with the ID file in the Vault and hKFC...OK\n");
+
+    SECKFMClose (&hKFC2, SECKFM_close_WriteIdFile, 0, NULL);
+
+
+    /* Terminate Domino and Notes. */
+
+
+
+    NSFDbClose(hDB);
+
+    NotesTerm();
+
+    /* End of intro program. */
+    PRINTLOG("\nProgram completed successfully.\n");
+    return (0);
 }
 
